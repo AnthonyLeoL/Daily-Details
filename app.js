69b66610417ca094fc6b6ff7555d8763ef0f0f0b
Anthony Lisa <Anthony.Lisa0@gmail.com>
2019-08-01 21:44:04 -0700

    jquery not working as seperate file, embedded as script tag as temporary fix

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//app goes here
+// app goes here
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var methodOverride = require('method-override')
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+app.get('/', function (req, res) {
+  res.render('landing')
+})
+app.get('/to-do', function (req, res) {
+  res.render('to-do')
+})
+
+app.listen(3000, function () {
+  console.log('online')
+})
