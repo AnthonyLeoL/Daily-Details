069e5b00c53349c708048536e8f89a7916a865e1
Anthony Lisa <Anthony.Lisa0@gmail.com>
2019-08-03 11:36:15 -0700

    removed comments from database model

--- models/todo_item.js
+++ models/todo_item.js
@@ -4,8 +4,7 @@ var todo_item = new mongoose.Schema({
   title: String,
   description: String,
   priority: String,
-  date_created: { type: Date, default: Date.now },
-  comments: []
+  date_created: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('todo', todo_item)
