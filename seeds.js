d186d4c0b12c8f46fccd47f05e9f870cd655c940
Anthony Lisa <Anthony.Lisa0@gmail.com>
2019-08-03 11:26:19 -0700

    changed seed data to include tutorial items

--- seeds.js
+++ seeds.js
@@ -3,9 +3,18 @@ var Todo = require('./models/todo_item')
 
 var data = [
   {
+    title: 'click to mark as done!',
+    description: 'you got more info!',
+    priority: 'MUST'
+  },
+  {
+    title: 'Hover to show the delete button!',
+    description: 'you got more info!',
+    priority: 'MUST'
+  },
+  {
     title: 'complete this web app',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    description: 'hover to show delete button!',
     priority: 'MUST'
   },
   {
